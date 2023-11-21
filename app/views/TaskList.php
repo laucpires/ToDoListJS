31fe555afba9334879181a33649a09bad060dff6
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-11-21 10:32:25 -0300

    Create TaskList.php

---
+++ app/views/TaskList.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include "/home/laura.pires/ToDoLau/includes/links.php"; ?>
+</head>
+
+<body>
+    <div id="to-do-list">
+    </div>
+    <button id="save-form" class="hide">Salvar</button>
+    <div id="modal">
+        <div class="modal-content">
+            <div class="header-modal">
+                <h3>Lista de Tarefas</h3>
+                <span class="close-modal">X</span>
+            </div>
+            <div class="task-list">
+            </div>
+            <button id="download-list">Download</button>
+        </div>
+    </div>
+</body>
+
+</html>
